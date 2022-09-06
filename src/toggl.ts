62bcd2ab38d0ea7517d4660a41b1d726cee13aeb
nabeliwo <nabeliwo@gmail.com>
2022-09-07 03:15:58 +0900

    :sparkles: toggl から正しい日付のデータを取得して時間を集計する

--- src/toggl.ts
+++ src/toggl.ts
@@ -1,18 +1,56 @@
 import axios from 'axios';
+import dayjs from 'dayjs';
+import isBetween from 'dayjs/plugin/isBetween';
 
-import {path, auth} from './config';
+dayjs.extend(isBetween);
+
+import {path, toggl} from './config';
+
+type TimeEntry = {
+  project_id: number;
+  duration: number;
+  stop: string;
+};
 
 export const getTimeEntries = async () => {
+  const today = dayjs().add(9, 'h'); // Cloud Functions の timezone を考慮した現在時刻
+  const startDate = today.subtract(2, 'd').format('YYYY-MM-DD'); // toggl の timezone を考慮して2日前からのデータを取る
+  const endDate = today.format('YYYY-MM-DD');
+  const query = `?start_date=${startDate}&end_date=${endDate}`;
+
   return axios
-    .get(path.toggl.timeEntries(), {
+    .get<TimeEntry[]>(path.toggl.timeEntries() + query, {
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Basic ${Buffer.from(
-          `${auth.toggl.user}:${auth.toggl.password}`
+          `${toggl.auth.user}:${toggl.auth.password}`
         ).toString('base64')}`,
       },
     })
     .then(res => {
-      return res.data;
+      const data: {[key: number]: number} = {};
+
+      // toggl のタイムゾーンを考慮した時間範囲(昨日の0時から今日の0時まで)
+      const min = dayjs(`${today.format('YYYY-MM-DD')} 00:00:00`)
+        .subtract(1, 'd')
+        .subtract(9, 'h');
+      const max = dayjs(`${today.format('YYYY-MM-DD')} 00:00:00`).subtract(
+        9,
+        'h'
+      );
+
+      res.data.forEach(item => {
+        if (!dayjs(item.stop).isBetween(min, max)) {
+          return;
+        }
+
+        if (data[item.project_id] === undefined) {
+          data[item.project_id] = item.duration;
+        } else {
+          data[item.project_id] = data[item.project_id] + item.duration;
+        }
+      });
+
+      return data;
     });
 };
